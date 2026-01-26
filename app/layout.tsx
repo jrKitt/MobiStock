6a96d6bc779ba4e8f5f681b42c76e9383e93e5bb
B-bsw <sorawitthassadorn15@gmail.com>
2026-01-26 13:22:32 +0700

    fix:font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from "next";
-import { Noto_Sans_Thai } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Prompt } from 'next/font/google'
+import './globals.css'
 
-const notoSansThai = Noto_Sans_Thai({
-  subsets: ["thai", "latin"],
-  variable: "--font-noto-sans-thai",
-  weight: ["400", "700"],
-  display: "swap",
-});
+const prompt = Prompt({
+    weight: ['300', '400', '500', '600', '700'],
+    subsets: ['latin', 'thai'],
+    variable: '--font-prompt',
+    display: 'swap',
+})
 
 export const metadata: Metadata = {
-  title: "MobiStock",
-  description: "MobiStock",
-};
+    title: 'MobiStock',
+    description: 'MobiStock',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="th">
-      <body className={`${notoSansThai.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="th">
+            <body className={`${prompt.className} antialiased`}>
+                {children}
+            </body>
+        </html>
+    )
 }
